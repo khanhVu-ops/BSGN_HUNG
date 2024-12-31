2404743179c3f341547185f5ab23b4fa10a26ffa
KhanhVu <vuvankhanh022002@gmail.com>
2024-12-31 13:36:52 +0700

    history

--- BSGNProject/Controller/HomeController/DoctorHome/DoctorHistory/Cell/DoctorHistoryTableViewCell.swift
+++ BSGNProject/Controller/HomeController/DoctorHome/DoctorHistory/Cell/DoctorHistoryTableViewCell.swift
@@ -11,13 +11,23 @@ class DoctorHistoryTableViewCell: BaseTableViewCell {
 
     @IBOutlet weak var borderView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var stateButton: UIButton!
+    @IBOutlet weak var completedButton: UIButton!
     @IBOutlet weak var infoView: UIView!
+    @IBOutlet weak var cancelledButton: UIButton!
+    @IBOutlet weak var avatarImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         borderView.backgroundColor = UIColor(hex: "#D7E6DF")
         infoView.backgroundColor = UIColor(hex: "#A4E5C7")
+        completedButton.isEnabled = false
+        cancelledButton.isEnabled = false
+
     }
 
-    
+    func bind(_ item: Appointment) {
+        completedButton.isHidden = item.status != "completed"
+        cancelledButton.isHidden = item.status != "cancelled"
+        nameLabel.text = item.patientName
+        avatarImageView.loadAvatar(url: "")
+    }
 }
--- BSGNProject/Controller/HomeController/DoctorHome/DoctorHistory/DoctorHistoryViewController.swift
+++ BSGNProject/Controller/HomeController/DoctorHome/DoctorHistory/DoctorHistoryViewController.swift
@@ -10,23 +10,40 @@ import UIKit
 class DoctorHistoryViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    private var data: [Appointment] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.register(UINib(nibName: "DoctorHistoryTableViewCell", bundle: nil), forCellReuseIdentifier: "DoctorHistoryTableViewCell")
         tableView.delegate = self
         tableView.dataSource = self
+        fetchData()
     }
 
+    func fetchData() {
+        let doctorID = Global.doctor?.id
+        let majorID = Global.doctor?.majorID ?? 0
+        data = []
+        GlobalService.shared.fetchAppointments(completion: { appointments in
+            // Xử lý danh sách các cuộc hẹn nhận được
+            for appointment in appointments {
+                if appointment.doctorID == doctorID && (appointment.status == "completed" || appointment.status == "cancelled") {
+                    self.data.append(appointment)
+                }
+            }
+            self.tableView.reloadData()
+        }, majorID: majorID)
+    }
 }
 
 extension DoctorHistoryViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DoctorHistoryTableViewCell", for: indexPath) as! DoctorHistoryTableViewCell
+        cell.bind(data[indexPath.row])
         return cell
     }
     
